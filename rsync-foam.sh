603320b82caf9a578c2a14a94fc5eb47546113e6
David Haberthür <email@davidhaberthuer.ch>
2024-11-20 14:48:16 +0100

    Options and folder as variables

--- rsync-foam.sh
+++ rsync-foam.sh
@@ -1,9 +1,10 @@
-folder='Schmid BFH Methylcellulose'
+folder="Schmid BFH Methylcellulose"
+options="--verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --include="*/""
 echo "2214 --> archive"
-rsync --verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --include="*/" --exclude="*.png" --exclude="*.mp4" --exclude="*.ORSSession" --exclude="*_rectmp.log" ~/2214/"$folder"/ ~/research_storage_uct/Archiv_Tape/"$folder"/
-# echo "2214 --> some research storage folder"
-# rsync --verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --include="*/" --exclude="*_rectmp.log" ~/2214/"$folder"/ ~/research_storage_SOMEWHERE
+rsync $options --exclude="*.png" --exclude="*.mp4" --exclude="*.ORSSession" --exclude="*_rectmp.log" ~/2214/"$folder"/ ~/research_storage_uct/Archiv_Tape/"$folder"/
+# echo "2214 --> Alinas folder somewhere"
+# rsync $options --exclude="*_rectmp.log" ~/2214/"$folder"/ ~/SOMEWHERE
 echo "2214 (everything but projections) -> anaklin25 FastSSD"
-rsync --verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --include="*/" --exclude="*.?if" --exclude="*_rectmp.log" ~/2214/"$folder"/ /media/habi/Fast_SSD/"$folder"/
+rsync $options --exclude="*.?if" --exclude="*_rectmp.log" ~/2214/"$folder"/ /media/habi/Fast_SSD/"$folder"/
 echo "Archive (log and roi files) --> repository subfolder"
-rsync --verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --include="*/" --include="*.?og" --include="*.?nc" --include="*.?oi" --exclude="*" ~/research_storage_uct/Archiv_Tape/"$folder"/ /home/habi/P/Documents/Schmid-Foam/logfiles/
+rsync $options --include="*.?og" --include="*.?nc" --include="*.?oi" --exclude="*" ~/research_storage_uct/Archiv_Tape/"$folder"/ /home/habi/P/Documents/Schmid-Foam/logfiles/
