403a968e45c9e3c85b7c24fc9b605baa7b0ee7a4
David Haberthür <email@davidhaberthuer.ch>
2025-07-08 17:42:46 +0200

    Also copy stuff to anaklin04

--- rsync-foam.sh
+++ rsync-foam.sh
@@ -3,8 +3,10 @@ options="--verbose --recursive --times --update --omit-dir-times --prune-empty-d
 echo "2214 --> archive"
 rsync $options --exclude="*.png" --exclude="*.mp4" --exclude="*.ORSSession" --exclude="*_rectmp.log" ~/2214/"$folder"/ ~/research_storage_uct/Archiv_Tape/"$folder"/
 # echo "2214 --> Alinas folder somewhere"
-# rsync $options --exclude="*_rectmp.log" ~/2214/"$folder"/ ~/SOMEWHERE
+# rsync $options ~/2214/"$folder"/ ~/SOMEWHERE
 echo "2214 (everything but projections) -> anaklin25 FastSSD"
-rsync $options --exclude="*.?if" --exclude="*_rectmp.log" ~/2214/"$folder"/ /media/habi/Fast_SSD/"$folder"/
+rsync $options --exclude="*.?if" ~/2214/"$folder"/ /media/habi/Fast_SSD/"$folder"/
+echo "2214 (everything but projections) -> anaklin04 FastSSD"
+rsync $options --exclude="*.?if" ~/2214/"$folder"/ ~/anaklin04-f/"$folder"/
 echo "Archive (log and roi files) --> repository subfolder"
 rsync $options --include="*.?og" --include="*.?nc" --include="*.?oi" --exclude="*" ~/research_storage_uct/Archiv_Tape/"$folder"/ /home/habi/P/Documents/Schmid-Foam/logfiles/
